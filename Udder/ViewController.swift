00d8254dd49eee8eb44c2aff451e62adfad5775f
Kristian Oppenheim <k.oppenheim@me.com>
2020-06-15 08:15:15 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Kristian Oppenheim on 2020-06-15.
+//  Copyright © 2020 Kristian Oppenheim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
